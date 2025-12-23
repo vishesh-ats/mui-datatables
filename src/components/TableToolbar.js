9ca2220288d92d4b3e301cbd831273c05920d29a
Vish M <vishesh.modi@atharvasystem.com>
2025-12-23 19:29:00 +0530

    v5.0.0 - Fix print functionality with react-to-print v3

--- src/components/TableToolbar.js
+++ src/components/TableToolbar.js
@@ -17,7 +17,7 @@ import { withStyles } from 'tss-react/mui';
 import { createCSVDownload, downloadCSV } from '../utils';
 import MuiTooltip from '@mui/material/Tooltip';
 
-export const defaultToolbarStyles = (theme) => ({
+export const defaultToolbarStyles = theme => ({
   root: {
     '@media print': {
       display: 'none',
@@ -137,9 +137,9 @@ class TableToolbar extends React.Component {
     iconActive: null,
     showSearch: Boolean(
       this.props.searchText ||
-      this.props.options.searchText ||
-      this.props.options.searchOpen ||
-      this.props.options.searchAlwaysOpen,
+        this.props.options.searchText ||
+        this.props.options.searchOpen ||
+        this.props.options.searchAlwaysOpen,
     ),
     searchText: this.props.searchText || null,
   };
@@ -160,15 +160,15 @@ class TableToolbar extends React.Component {
       columnOrderCopy = columns.map((item, idx) => idx);
     }
 
-    data.forEach((row) => {
+    data.forEach(row => {
       let newRow = { index: row.index, data: [] };
-      columnOrderCopy.forEach((idx) => {
+      columnOrderCopy.forEach(idx => {
         newRow.data.push(row.data[idx]);
       });
       dataToDownload.push(newRow);
     });
 
-    columnOrderCopy.forEach((idx) => {
+    columnOrderCopy.forEach(idx => {
       columnsToDownload.push(columns[idx]);
     });
 
@@ -182,7 +182,7 @@ class TableToolbar extends React.Component {
           row.index = index;
 
           return {
-            data: row.data.map((column) => {
+            data: row.data.map(column => {
               i += 1;
 
               // if we have a custom render, which will appear as a react element, we must grab the actual value from data
@@ -190,18 +190,18 @@ class TableToolbar extends React.Component {
               // TODO: Create a utility function for checking whether or not something is a react object
               let val =
                 typeof column === 'object' && column !== null && !Array.isArray(column)
-                  ? find(data, (d) => d.index === row.dataIndex).data[i]
+                  ? find(data, d => d.index === row.dataIndex).data[i]
                   : column;
-              val = typeof val === 'function' ? find(data, (d) => d.index === row.dataIndex).data[i] : val;
+              val = typeof val === 'function' ? find(data, d => d.index === row.dataIndex).data[i] : val;
               return val;
             }),
           };
         });
 
         dataToDownload = [];
-        filteredDataToDownload.forEach((row) => {
+        filteredDataToDownload.forEach(row => {
           let newRow = { index: row.index, data: [] };
-          columnOrderCopy.forEach((idx) => {
+          columnOrderCopy.forEach(idx => {
             newRow.data.push(row.data[idx]);
           });
           dataToDownload.push(newRow);
@@ -210,9 +210,9 @@ class TableToolbar extends React.Component {
 
       // now, check columns:
       if (options.downloadOptions.filterOptions.useDisplayedColumnsOnly) {
-        columnsToDownload = columnsToDownload.filter((_) => _.display === 'true');
+        columnsToDownload = columnsToDownload.filter(_ => _.display === 'true');
 
-        dataToDownload = dataToDownload.map((row) => {
+        dataToDownload = dataToDownload.map(row => {
           row.data = row.data.filter((_, index) => columns[columnOrderCopy[index]].display === 'true');
           return row;
         });
@@ -221,9 +221,9 @@ class TableToolbar extends React.Component {
     createCSVDownload(columnsToDownload, dataToDownload, options, downloadCSV);
   };
 
-  setActiveIcon = (iconName) => {
+  setActiveIcon = iconName => {
     this.setState(
-      (prevState) => ({
+      prevState => ({
         showSearch: this.isSearchShown(iconName),
         iconActive: iconName,
         prevIconActive: prevState.iconActive,
@@ -247,7 +247,7 @@ class TableToolbar extends React.Component {
     );
   };
 
-  isSearchShown = (iconName) => {
+  isSearchShown = iconName => {
     if (this.props.options.searchAlwaysOpen) {
       return true;
     }
@@ -297,7 +297,7 @@ class TableToolbar extends React.Component {
     }));
   };
 
-  handleSearch = (value) => {
+  handleSearch = value => {
     this.setState({ searchText: value });
     this.props.searchTextUpdate(value);
   };
@@ -386,7 +386,7 @@ class TableToolbar extends React.Component {
               <IconButton
                 aria-label={search}
                 data-testid={search + '-iconButton'}
-                ref={(el) => (this.searchButton = el)}
+                ref={el => (this.searchButton = el)}
                 classes={{ root: this.getActiveIcon(classes, 'search') }}
                 disabled={options.search === 'disabled'}
                 onClick={this.handleSearchIconClick}>
